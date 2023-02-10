0dac233e78aecc09efe6d2bd583463594dcaa453
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2023-02-10 18:24:24 -0300

    User routes.

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,11 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 const Service = require('../services/services');
+const { validationResult } = require('express-validator');
 
 const getAll = async (req, res, next) => {
   console.log('Get all users.');
+  // #swagger.tags = ['Users']
 
   const result = await mongodb.getDb().db().collection('users').find();
   result.toArray().then((lists) => {
@@ -14,6 +16,7 @@ const getAll = async (req, res, next) => {
 
 const regNewUser = async (req, res, next) => {
   console.log('Register new User.');
+  // #swagger.tags = ['Users']
 
   let newUser  = {
     name: req.body.name,
@@ -22,16 +25,59 @@ const regNewUser = async (req, res, next) => {
     attendsTo: []
   };
 
-  newUser = Service.cleanObject(newUser);
+  const errors = validationResult(req);
 
-  const insertResult = await mongodb.getDb().db().collection('users').insertOne(newUser);
-
-  res.setHeader('Content-Type', 'application/json');
-  if (insertResult.acknowledged) {
-    res.status(201).json(insertResult);
+  if (!errors.isEmpty()) {
+    res.status(500).json(errors)
   } else {
-    res.status(500).json(insertResult.error);
+    const insertResult = await mongodb.getDb().db().collection('users').insertOne(newUser);
+
+    res.setHeader('Content-Type', 'application/json');
+    if (insertResult.acknowledged) {
+      res.status(201).json(insertResult);
+    } else {
+      res.status(500).json(insertResult.error);
+    }
   }
 };
 
-module.exports = { getAll, regNewUser };
+const updateUser = async (req, res, next) => {
+  console.log('Update user.')
+  // #swagger.tags = ['Users']
+
+  const userId = new ObjectId(req.params.id);
+  let userInfo = {
+    name: req.body.name,
+    password: req.body.password,
+    profession: req.body.profession
+  }
+  
+  userInfo = Service.cleanObject(userInfo);
+
+  const updateResult = await mongodb.getDb().db().collection('events').updateOne({ _id: userId }, { $set: userInfo });
+
+  res.setHeader('Content-Type', 'application/json');
+  if (updateResult.acknowledged) {
+    res.status(201).json(updateResult);
+  } else {
+    res.status(500).json(updateResult.error);
+  }
+}
+
+const deleteUser = async (req, res, next) => {
+  console.log('Delete user.')
+  // #swagger.tags = ['Users']
+
+  const userId = new ObjectId(req.params.id);
+
+  const deleteResult = await mongodb.getDb().db().collection('users').deleteOne({ _id: userId });
+
+  res.setHeader('Content-Type', 'application/json');
+  if (deleteResult.acknowledged) {
+    res.status(204).json(deleteResult);
+  } else {
+    res.status(500).json(deleteResult.error | 'Error');
+  }
+}
+
+module.exports = { getAll, regNewUser, deleteUser };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { signupValidation } = require('../services/validate')
 
 const userController = require('../controllers/user');
 
 router.get('/', userController.getAll);
 
-router.post('/', userController.regNewUser);
+router.post('/', signupValidation, userController.regNewUser);
+
+router.put('/:id', userController.regNewUser);
+
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
