91b1d221deeb7c1988824955695a08d0290e1fab
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2023-02-10 15:37:18 -0300

    Swagger config update.

--- swagger.js
+++ swagger.js
@@ -1,37 +1,87 @@
 const swaggerAutogen = require('swagger-autogen')();
-const title = 'Events API';
-const description = 'An events API build for CSE 341 at BYUI. Author: Alvaro B. Godoy';
 
-const docDev = {
+let docDev = {
   info: {
-    title: title,
-    description: description,
+    title: 'Events API',
+    description: 'An events API build for CSE 341 at BYUI. Author: Alvaro B. Godoy',
   },
   host: 'localhost:8080',
   schemes: ['http'],
+  '@definitions': {
+    event: {
+      type: "object",
+      properties: {
+        name: {
+          type: "string",
+          example: "Tech Conference"
+        },
+        description: {
+          type: "string",
+          example: "A conference focused on the latest advancements in technology"
+        },
+        date: {
+          type: "string",
+          format: "date",
+          example: "2023-05-12"
+        },
+        location: {
+          type: "string",
+          example: "San Francisco, CA"
+        },
+        speakers: {
+          type: "array",
+          items: {
+            type: "string",
+            example: "John Doe"
+          }
+        },
+        contactInfo: {
+          type: "string",
+          example: "techconference@example.com"
+        },
+        attendees: {
+          type: "array",
+          items: {
+            type: "string",
+            example: "63e18be2a421f7d29426070b"
+          }
+        }
+      }
+    },
+    user: {
+      type: "object",
+      properties: {
+        name: {
+          type: "string",
+          example: "John Doe"
+        },
+        password: {
+          type: "string",
+          example: "password1"
+        },
+        profession: {
+          type: "string",
+          example: "Software Engineer"
+        },
+        attendsTo: {
+          type: "array",
+          items: {
+            type: "string",
+            example: "63dee3ff32595491a090e934"
+          }
+        }
+      }
+    }
+  }
 };
 
-const doc = {
-  info: {
-    title: title,
-    description: description,
-  },
-  host: 'cse341-events.onrender.com',
-  schemes: ['https'],
-};
+let doc = docDev;
+doc.host = 'cse341-events.onrender.com';
+doc.schemes = ['https'];
 
 const outputFile = './swagger.json'
 const outputFileDev = './swagger-dev.json'
 const endpointsFiles = ['./routes/index.js']
 
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
-
 swaggerAutogen(outputFile, endpointsFiles, doc);
-
 swaggerAutogen(outputFileDev, endpointsFiles, docDev);
-
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-//     require('./index')           // Your project's root file
-// })
